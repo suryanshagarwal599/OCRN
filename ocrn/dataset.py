1d93cf328184b63a57d2dfc273d26e22bd214ba9
suryanshagarwal599 <30744804+suryanshagarwal599@users.noreply.github.com>
2020-01-26 15:29:42 +0530

    Add files via upload

---
+++ ocrn/dataset.py
@@ -0,0 +1,34 @@
+from pybrain.datasets import SupervisedDataSet
+import numpy as np
+import feature as ft
+import fileinput
+
+class dataset:
+	# Initialize the dataset with input and label size
+	def __init__(self, inputsize, labelsize):
+		self.inputsize = inputsize
+		self.labelsize = labelsize
+		self.DS = SupervisedDataSet(self.inputsize, self.labelsize)
+	
+	# Adds data to existing training dataset
+	def addTrainingData(self,inputdata, labeldata):
+		try:
+			if inputdata.size == self.inputsize and labeldata.size == self.labelsize:
+				self.DS.appendLinked(inputdata, labeldata)
+				return 1
+		except AttributeError:
+			print "Input error."
+			return 0
+	
+	def getTrainingDataset(self):
+		return self.DS
+	
+	def generateDataSet(self):
+		for line in fileinput.input(['data/inputdata']):
+			x = line.split(':')
+	
+			self.addTrainingData(ft.feature.getImageFeatureVector(x[0]),np.array([int(x[1])]))
+			
+	
+	
+		return 1
---
+++ ocrn/neuralnet.py
@@ -0,0 +1,39 @@
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+
+class neuralnet:
+	def __init__(self, *args):
+		if len(args) >=2 :
+			self.nnet = buildNetwork(*args)
+			self.inputdimension = args[0]
+			self.outputdimension = args[len(args)-1]
+			self.nnet.sortModules()
+			
+		else:
+			print "Number of layers must be greater than or equal to two\n"
+
+	# Loads the training data into neural network before training.
+	def loadTrainingData(self, trainingdataset):
+		if trainingdataset.getDimension('input') == self.inputdimension and \
+			trainingdataset.getDimension('target') == self.outputdimension:
+				self.trainer = BackpropTrainer(self.nnet, trainingdataset)
+				return 1
+		else:
+			print "Dataset-Network size mismatch\n"
+			return 0
+
+	# Train the neural network 'n' times with loaded training data.
+	def teach(self, n):
+		for i in range (1, n+1):
+			print str(i) + " : " + str(self.trainer.train())
+
+	# Activate the Neural Network with test data. Returns calculated output.
+	def activate(self, testdata):
+		if testdata.size == self.inputdimension:
+			x = self.nnet.activate(testdata)
+			return int(round(x[0],0))
+		else:
+			print "Test data error\n"
+			return 0
+
+
